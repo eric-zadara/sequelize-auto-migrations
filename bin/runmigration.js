3be2a28455019e3a4dd627fba4a9b9564aa34824
eric-zadara <eric@zadarastorage.com>
2018-11-08 10:37:28 -0800

    chore: code cleanup

--- bin/runmigration.js
+++ bin/runmigration.js
@@ -1,92 +1,90 @@
 #!/bin/node
 
-const path              = require("path");
-const commandLineArgs   = require('command-line-args');
-const fs                = require("fs");
-const Async             = require("async");
+const path = require('path')
+const commandLineArgs = require('command-line-args')
+const fs = require('fs')
+const Async = require('async')
 
-const migrate           = require("../lib/migrate");
+const migrate = require('../lib/migrate')
 
 const optionDefinitions = [
-    { name: 'rev', alias: 'r', type: Number, description: 'Set migration revision (default: 0)', defaultValue: 0 },
-    { name: 'pos', alias: 'p', type: Number, description: 'Run first migration at pos (default: 0)', defaultValue: 0 },
-    { name: 'one', type: Boolean, description: 'Do not run next migrations', defaultValue: false },
-    { name: 'list', alias: 'l', type: Boolean, description: 'Show migration file list (without execution)', defaultValue: false },
-    { name: 'migrations-path', type: String, description: 'The path to the migrations folder' },
-    { name: 'models-path', type: String, description: 'The path to the models folder' },
-    { name: 'help', type: Boolean, description: 'Show this message' }
-];
+  { name: 'rev', alias: 'r', type: Number, description: 'Set migration revision (default: 0)', defaultValue: 0 },
+  { name: 'pos', alias: 'p', type: Number, description: 'Run first migration at pos (default: 0)', defaultValue: 0 },
+  { name: 'one', type: Boolean, description: 'Do not run next migrations', defaultValue: false },
+  { name: 'list', alias: 'l', type: Boolean, description: 'Show migration file list (without execution)', defaultValue: false },
+  { name: 'migrations-path', type: String, description: 'The path to the migrations folder' },
+  { name: 'models-path', type: String, description: 'The path to the models folder' },
+  { name: 'help', type: Boolean, description: 'Show this message' }
+]
 
-const options = commandLineArgs(optionDefinitions);
+const options = commandLineArgs(optionDefinitions)
 
-if(!process.env.PWD){
-    process.env.PWD = process.cwd()
+if (!process.env.PWD) {
+  process.env.PWD = process.cwd()
 }
 
-let migrationsDir = path.join(process.env.PWD, options['migrations-path'] || 'migrations'),
-    modelsDir     = path.join(process.env.PWD, options['models-path'] || 'models');
+let migrationsDir = path.join(process.env.PWD, options['migrations-path'] || 'migrations')
 
-if (options.help)
-{
-    console.log("Simple sequelize migration execution tool\n\nUsage:");
-    optionDefinitions.forEach((option) => {
-        let alias = (option.alias) ? ` (-${option.alias})` : '\t';
-        console.log(`\t --${option.name}${alias} \t${option.description}`);
-    });
-    process.exit(0);
+let modelsDir = path.join(process.env.PWD, options['models-path'] || 'models')
+
+if (options.help) {
+  console.log('Simple sequelize migration execution tool\n\nUsage:')
+  optionDefinitions.forEach((option) => {
+    let alias = (option.alias) ? ` (-${option.alias})` : '\t'
+    console.log(`\t --${option.name}${alias} \t${option.description}`)
+  })
+  process.exit(0)
 }
 
-const Sequelize = require("sequelize");
-const sequelize = require(modelsDir).sequelize;
-const queryInterface = sequelize.getQueryInterface();
+// const Sequelize = require('sequelize')
+const sequelize = require(modelsDir).sequelize
+const queryInterface = sequelize.getQueryInterface()
 
 // execute all migration from
-let fromRevision = options.rev;
-let fromPos = parseInt(options.pos);
-let stop = options.one;
+let fromRevision = options.rev
+let fromPos = parseInt(options.pos)
+let stop = options.one
 
 let migrationFiles = fs.readdirSync(migrationsDir)
 // filter JS files
   .filter((file) => {
-    return (file.indexOf('.') !== 0) && (file.slice(-3) === '.js');
+    return (file.indexOf('.') !== 0) && (file.slice(-3) === '.js')
   })
 // sort by revision
-  .sort( (a, b) => {
-      let revA = parseInt( path.basename(a).split('-',2)[0]),
-          revB = parseInt( path.basename(b).split('-',2)[0]);
-      if (revA < revB) return -1;
-      if (revA > revB) return 1;
-      return 0;
+  .sort((a, b) => {
+    let revA = parseInt(path.basename(a).split('-', 2)[0])
+
+    let revB = parseInt(path.basename(b).split('-', 2)[0])
+    if (revA < revB) return -1
+    if (revA > revB) return 1
+    return 0
   })
 // remove all migrations before fromRevision
   .filter((file) => {
-      let rev = parseInt( path.basename(file).split('-',2)[0]);
-      return (rev >= fromRevision);
-  });
-  
-console.log("Migrations to execute:");  
+    let rev = parseInt(path.basename(file).split('-', 2)[0])
+    return (rev >= fromRevision)
+  })
+
+console.log('Migrations to execute:')
 migrationFiles.forEach((file) => {
-    console.log("\t"+file);
-});
+  console.log('\t' + file)
+})
 
-if (options.list)
-    process.exit(0);
+if (options.list) { process.exit(0) }
 
+Async.eachSeries(migrationFiles,
+  function (file, cb) {
+    console.log('Execute migration from file: ' + file)
+    migrate.executeMigration(queryInterface, path.join(migrationsDir, file), fromPos, (err) => {
+      if (stop) { return cb('Stopped') }
 
-Async.eachSeries(migrationFiles, 
-    function (file, cb) {
-        console.log("Execute migration from file: "+file);
-        migrate.executeMigration(queryInterface, path.join(migrationsDir, file), fromPos, (err) => {
-            if (stop)
-                return cb("Stopped");
-                
-            cb(err);
-        });
-        // set pos to 0 for next migration
-        fromPos = 0;
-    },
-    function(err) {
-        console.log(err);
-        process.exit(0);
-    }
-);
+      cb(err)
+    })
+    // set pos to 0 for next migration
+    fromPos = 0
+  },
+  function (err) {
+    console.log(err)
+    process.exit(0)
+  }
+)
